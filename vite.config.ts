1188d2f3a4aa767115d584ef6756d437664fc358
Myungjin Ki <myungjinki91@gmail.com>
2024-10-30 18:29:00 +0900

    Update deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://myungjinki91.github.io/toy-pomodoro",
 });
